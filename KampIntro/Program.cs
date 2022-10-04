1a98700aeb234b43358efd6f1c749c7827338fcd
Oğuz Kağan ER <oguzkaganerr@gmail.com>
2022-10-04 18:24:22 +0300

    04.10.2022 - 18:24 - Ders Sonu

--- KampIntro/Program.cs
+++ KampIntro/Program.cs
@@ -1,5 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine
+    (@"Hello, World!
+            Hello, World!
+Hello, World!"
+    );
+
+Console.ReadLine();
 
 class MyClass
 {
