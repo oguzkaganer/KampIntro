672476c6a101a3ed56ee62d23d5ec4ca6fc3ec88
Oğuz Kağan ER <oguzkaganerr@gmail.com>
2022-10-04 17:33:49 +0300

    ClassMetotDemo projesi eklendi.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class MusteriManager
+    {
+        public static void Ekle(List<Musteri> liste,Musteri musteri)
+        {
+            liste.Add(musteri);
+        }
+
+        public static void Sil(List<Musteri> liste, Musteri musteri)
+        {
+            liste.Remove(musteri);
+        }
+
+        public static void Listele(List<Musteri> musteriler)
+        {
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Id + "-"+musteri.Ad+"-"+musteri.Soyad);
+            }
+        }
+
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ClassMetotDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Oğuz";
+            musteri1.Soyad = "ER";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Kadir";
+            musteri2.Soyad = "Özdemir";
+
+            List<Musteri> musteriler = new List<Musteri>();
+
+            MusteriManager.Ekle(musteriler, musteri1);
+            MusteriManager.Ekle(musteriler, musteri2);
+            MusteriManager.Listele(musteriler);
+            Console.WriteLine();
+            MusteriManager.Sil(musteriler, musteri2);
+            MusteriManager.Listele(musteriler);
+
+
+            Console.ReadLine();
+        }
+    }
+}
