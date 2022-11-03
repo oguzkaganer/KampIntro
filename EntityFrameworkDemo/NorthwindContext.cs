fa9b6ad11023e05744d419ff85021fabb3ea8611
Oğuz Kağan ER <oguzkaganerr@gmail.com>
2022-11-03 23:13:48 +0300

    03.11.2022 - 23:13

---
+++ EntityFrameworkDemo/NorthwindContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDemo
+{
+    public class NorthwindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Northwind;Trusted_Connection=true");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.HasDefaultSchema("dbo");
+            //modelBuilder.Entity<Personel>().ToTable("Employees","dbo");
+            modelBuilder.Entity<Personel>().ToTable("Employees");
+
+            modelBuilder.Entity<Personel>().Property(p => p.Id).HasColumnName("EmployeeID");
+            modelBuilder.Entity<Personel>().Property(p => p.Surname).HasColumnName("LastName");
+            modelBuilder.Entity<Personel>().Property(p => p.Name).HasColumnName("FirstName");
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ EntityFrameworkDemo/Program.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using System.Linq;
+
+namespace EntityFrameworkDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //GetAll();
+            GetProductsByCategory(2);
+
+
+
+
+            Console.ReadLine();
+        }
+
+        private static void GetAll()
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                foreach (var product in context.Products)
+                {
+                    Console.WriteLine(product.ProductName);
+                }
+            }
+        }
+
+        private static void GetProductsByCategory(int categoryId)
+        {
+            NorthwindContext context = new NorthwindContext();
+            int i;
+
+
+            //var result = context.Products.Where(p=>p.CategoryID==categoryId).ToList();
+            //
+            //
+            //i = 1;
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(i+". "+item.ProductName);
+            //    i++;
+            //}
+
+            var result2 = context.Products
+                .Join(context.Categories, p => p.CategoryID, c => c.CategoryID, (p, c) => new { p, c })
+                .Where(p => p.p.CategoryID == categoryId)
+                .ToList();
+
+            i = 1;
+            foreach (var item in result2)
+            {
+                    Console.WriteLine(i + ". " + item.p.ProductName + " - " + item.c.CategoryName);
+                    i++;
+                
+            }
+
+
+            //var result3 = from product in context.Products
+            //              join category in context.Categories on product.CategoryID equals category.CategoryID
+            //              where product.CategoryID == categoryId
+            //              select new { product, category };
+            //i = 1;
+            //foreach (var item in result3)
+            //{
+            //    Console.WriteLine(i + ". " + item.product.ProductName + " - " + item.category.CategoryName);
+            //    i++;
+            //}
+
+
+        }
+    }
+}
